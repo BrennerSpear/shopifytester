e6ffbf7b5e66ac213ec7e06695c737dbac7c11e4
BrennerSpear <Blspear@gmail.com>
2015-08-07 15:18:42 -0700

    changed to index

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,11 @@ Rails.application.routes.draw do
     get 'logout' => :destroy, :as => :logout
   end
 
+  get 'index' => 'home#index'
+
   root :to => 'home#index'
-  
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
