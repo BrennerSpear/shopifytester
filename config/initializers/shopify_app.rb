c6db2f4bc995290327169637fd3295554949a39d
BrennerSpear <Blspear@gmail.com>
2015-08-08 10:49:47 -0700

    changed redirect URI

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.api_key = ENV["shopify_api_key"]
   config.secret = ENV["shopify_secret"]
-  config.redirect_uri = "https://shopifytester.herokuapp.com/login"
+  config.redirect_uri = "https://shopifytester.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
